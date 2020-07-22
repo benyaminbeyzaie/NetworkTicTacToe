d373aa7f3625cad0912f1ee74f783ce8756b9753
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-22 23:56:00 +0430

    add StateManger to constructor

--- src/main/java/view/state/State.java
+++ src/main/java/view/state/State.java
@@ -1,14 +1,17 @@
 package view.state;
 
 import view.config.configmodels.ClientConfig;
+import view.display.StateManager;
 
 import javax.swing.*;
 
 public abstract class State extends JPanel {
     private ClientConfig config;
+    StateManager stateManager;
 
-    State(ClientConfig clientConfig){
+    State(ClientConfig clientConfig, StateManager stateManager){
         this.config = clientConfig;
+        this.stateManager = stateManager;
     }
 
     public ClientConfig getConfig() {
