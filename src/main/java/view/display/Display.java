70d77674283d4fcda4b9b069ec5c5bcd861efa43
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-22 23:20:25 +0430

    make display extends JFrame

--- src/main/java/view/display/Display.java
+++ src/main/java/view/display/Display.java
@@ -1,4 +1,32 @@
 package view.display;
 
-public class Display {
+import view.config.configmodels.ClientConfig;
+import view.config.configmodels.Config;
+import view.state.LoginState;
+import view.state.State;
+
+import javax.swing.*;
+
+public class Display extends JFrame {
+    private static Display display;
+
+    private Display(ClientConfig clientConfig){
+        super();
+        setTitle("Tic-Tac-Toe");
+        setSize(clientConfig.getWidth(), clientConfig.getHeight());
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static Display getInstance(ClientConfig clientConfig){
+        if (display == null) display = new Display(clientConfig);
+        return display;
+    }
+
+    public void setCurrentState(State state){
+        setContentPane(state);
+    }
+
 }
