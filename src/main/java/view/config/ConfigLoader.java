a568b933d363235334da80d9215d8c88c50ba7fd
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-22 17:33:12 +0430

    create a full ConfigLoader class

--- src/main/java/view/config/ConfigLoader.java
+++ src/main/java/view/config/ConfigLoader.java
@@ -1,4 +1,70 @@
 package view.config;
 
+import view.config.configmodels.ClientConfig;
+import view.config.configmodels.Config;
+import view.config.configmodels.ServerConfig;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class ConfigLoader {
+    private ConfigLoader configLoader;
+
+    private ConfigLoader(){}
+
+    public ConfigLoader getInstance(){
+        if (configLoader == null) configLoader = new ConfigLoader();
+        return configLoader;
+    }
+
+    public ClientConfig loadClientConfig(String address) throws FileNotFoundException, XMLStreamException {
+        FileInputStream xmlFile = new FileInputStream(address);
+        return (ClientConfig)parseClientConfigXML(xmlFile);
+    }
+
+    public ServerConfig loadServerConfig(String address) throws FileNotFoundException, XMLStreamException {
+        FileInputStream xmlFile = new FileInputStream(address);
+        return (ServerConfig) parseClientConfigXML(xmlFile);
+    }
+
+    private Config parseClientConfigXML(FileInputStream fileInputStream) throws XMLStreamException {
+        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+        XMLEventReader xmlEventReader = xmlInputFactory.createXMLEventReader(fileInputStream);
+        Config config = new ClientConfig();
+        while (xmlEventReader.hasNext()){
+            XMLEvent xmlEvent = xmlEventReader.nextEvent();
+            if (xmlEvent.isStartElement()){
+                StartElement startElement = xmlEvent.asStartElement();
+                switch (startElement.getName().getLocalPart()) {
+                    case "ip":
+                        xmlEvent = xmlEventReader.nextEvent();
+                        config.setIp(xmlEvent.asCharacters().getData());
+                        break;
+                    case "port":
+                        xmlEvent = xmlEventReader.nextEvent();
+                        config.setPort(Integer.parseInt(xmlEvent.asCharacters().getData()));
+                        break;
+                    case "window_size":
+                        xmlEventReader.nextTag();
+                        xmlEvent = xmlEventReader.nextEvent();
+                        ((ClientConfig)config).setWidth(Integer.parseInt(xmlEvent.asCharacters().getData()));
+                        xmlEventReader.nextTag();
+                        xmlEventReader.nextTag();
+                        xmlEvent = xmlEventReader.nextEvent();
+                        ((ClientConfig)config).setHeight(Integer.parseInt(xmlEvent.asCharacters().getData()));
+                        break;
+                    case "title":
+                        xmlEvent = xmlEventReader.nextEvent();
+                        ((ClientConfig)config).setTitle(xmlEvent.asCharacters().getData());
+                        break;
+                }
+            }
+        }
+        return config;
+    }
 }
