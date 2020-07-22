a3798ba4cf06a708f5f2c73a28178d7291d3b07f
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-23 01:40:57 +0430

    add draw login background function

--- src/main/java/view/state/State.java
+++ src/main/java/view/state/State.java
@@ -1,9 +1,11 @@
 package view.state;
 
+import view.assets.Assets;
 import view.config.configmodels.ClientConfig;
 import view.display.StateManager;
 
 import javax.swing.*;
+import java.awt.*;
 
 public abstract class State extends JPanel {
     private ClientConfig config;
@@ -20,5 +22,14 @@ public abstract class State extends JPanel {
 
     public void setConfig(ClientConfig config) {
         this.config = config;
+    }
+
+    void drawLoginStateBackground(Graphics g){
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(0, 0, super.getWidth()/2, super.getHeight());
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2.drawImage(Assets.TicTacTaoLogo, super.getWidth() / 4 - Assets.TicTacTaoLogo.getWidth() / 2,
+                super.getHeight() / 2 - Assets.TicTacTaoLogo.getHeight() / 2, null);
     }
 }
