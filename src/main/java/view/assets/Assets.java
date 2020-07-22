0847f28c94cc7e1930512562220c6dd7fd95cd33
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-22 22:34:05 +0430

    add logo image to assets

--- src/main/java/view/assets/Assets.java
+++ src/main/java/view/assets/Assets.java
@@ -1,4 +1,11 @@
 package view.assets;
 
+import java.awt.image.BufferedImage;
+
 public class Assets {
+    public static BufferedImage TicTacTaoLogo;
+
+    public static void init(){
+        TicTacTaoLogo = ImageLoader.loadImage("src/main/resources/images/TicTacTaoLogo.png");
+    }
 }
