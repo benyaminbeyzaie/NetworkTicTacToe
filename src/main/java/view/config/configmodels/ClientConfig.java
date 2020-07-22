83488ef186125d34bf74ec1d46c46f1c412e425e
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-22 17:32:29 +0430

    add fields to the class

---
+++ src/main/java/view/config/configmodels/ClientConfig.java
@@ -0,0 +1,33 @@
+package view.config.configmodels;
+
+import view.config.ConfigLoader;
+
+public class ClientConfig extends Config {
+    private String title;
+    private int width;
+    private int height;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
