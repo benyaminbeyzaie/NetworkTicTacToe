45e3f6ce0ee2754b22332d3650ee7f9755db68d6
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-22 22:33:17 +0430

    add constants font class

---
+++ src/main/java/view/constants/Fonts.java
@@ -0,0 +1,9 @@
+package view.constants;
+
+import java.awt.*;
+
+public class Fonts {
+    public static final Font STANDARD_FONT = new Font("Verdana", Font.PLAIN, 16);
+    public static final Font BIG_FONT = new Font("Verdana", Font.BOLD, 20);
+    public static final Font SMALL_FONT = new Font("Verdana", Font.PLAIN, 14);
+}
