e397ca6e3e19dbdc5e98788c80ff50927aa97746
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-22 22:52:01 +0430

    complete UI

--- src/main/java/view/state/LoginState.java
+++ src/main/java/view/state/LoginState.java
@@ -1,4 +1,78 @@
 package view.state;
 
-public class LoginState {
+
+import view.assets.Assets;
+import view.config.configmodels.ClientConfig;
+import view.constants.Fonts;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginState extends State implements StatePage{
+
+    public LoginState(ClientConfig config){
+        super(config);
+        setLayout(null);
+
+        // Login label
+        JLabel loginLabel = new JLabel("Login");
+        loginLabel.setFont(Fonts.BIG_FONT);
+        loginLabel.setBounds( 3 * super.getConfig().getWidth() / 4 - (getFontMetrics(Fonts.BIG_FONT).stringWidth(loginLabel.getText()) / 2 + 5), 20,
+                100, 100);
+
+
+        //Username text field
+        JTextField usernameField = new JTextField(18);
+        usernameField.setBounds(3 * super.getConfig().getWidth() / 4 - 105, 200 ,200, 30);
+
+        //Password text field
+        JTextField passwordField = new JTextField(18);
+        passwordField.setBounds(3 * super.getConfig().getWidth() / 4 - 105, 240 ,200, 30);
+
+        //Login button
+        JButton loginButton = new JButton();
+        loginButton.setText("Login");
+        loginButton.setBounds(3 * super.getConfig().getWidth() / 4 - 105, 280, 200, 30);
+        loginButton.setBackground(Color.ORANGE);
+        loginButton.setBorder(null);
+
+        //Sign up button
+        JButton signUpButton = new JButton();
+        signUpButton.setText("Sign up");
+        signUpButton.setBounds(3 * super.getConfig().getWidth() / 4 - 200 + 95, 320, 95, 30);
+        signUpButton.setBackground(Color.GREEN);
+        signUpButton.setBorder(null);
+
+        //Exit button
+        JButton exitButton = new JButton();
+        exitButton.setText("Exit");
+        exitButton.setBounds(3 * super.getConfig().getWidth() / 4 + 95 + 10 - 200 + 95, 320, 95, 30);
+        exitButton.setBackground(Color.RED);
+        exitButton.setBorder(null);
+
+
+
+        add(loginLabel);
+        add(usernameField);
+        add(passwordField);
+        add(loginButton);
+        add(signUpButton);
+        add(exitButton);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        render(g);
+    }
+
+    @Override
+    public void render(Graphics g) {
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(0, 0, super.getWidth()/2, super.getHeight());
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2.drawImage(Assets.TicTacTaoLogo, super.getWidth() / 4 - Assets.TicTacTaoLogo.getWidth() / 2,
+                super.getHeight() / 2 - Assets.TicTacTaoLogo.getHeight() / 2, null);
+    }
 }
