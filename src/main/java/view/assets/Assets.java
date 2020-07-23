53a32eaf9be99c85a23703b9c31589c928c6d3b6
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-23 14:06:05 +0430

    add star and circle images

--- src/main/java/view/assets/Assets.java
+++ src/main/java/view/assets/Assets.java
@@ -4,8 +4,13 @@ import java.awt.image.BufferedImage;
 
 public class Assets {
     public static BufferedImage TicTacTaoLogo;
+    public static BufferedImage redCircle, blackCircle, redStar, blackStar;
 
     public static void init(){
         TicTacTaoLogo = ImageLoader.loadImage("src/main/resources/images/TicTacTaoLogo.png");
+        redCircle = ImageLoader.loadImage("src/main/resources/images/redCircle.png");
+        blackCircle = ImageLoader.loadImage("src/main/resources/images/blackCircle.png");
+        redStar = ImageLoader.loadImage("src/main/resources/images/redStar.png");
+        blackStar = ImageLoader.loadImage("src/main/resources/images/blackStar.png");
     }
 }
