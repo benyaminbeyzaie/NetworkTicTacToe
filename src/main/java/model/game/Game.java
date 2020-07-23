be50f906605f85654126fe5f46d015ae41a5f86c
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-23 22:53:47 +0430

    add basics of Game class

--- src/main/java/model/game/Game.java
+++ src/main/java/model/game/Game.java
@@ -1,4 +1,54 @@
 package model.game;
 
+import controller.GameController;
+import model.player.Player;
+
+import java.util.Random;
+
 public class Game {
+    private int[] board;
+    private GameController controller;
+    private Player currentPlayer, opponentPlayer;
+
+
+    public Game(Player playerOne, Player playerTwo){
+        initializePlayers(playerOne, playerTwo);
+        initializeBoard();
+        controller = new GameController();
+    }
+
+    private void initializePlayers(Player playerOne, Player playerTwo) {
+        Random random = new Random();
+        int rand = random.nextInt(2);
+        if (rand == 0){
+            currentPlayer = playerOne;
+            opponentPlayer = playerTwo;
+        }else {
+            currentPlayer = playerTwo;
+            opponentPlayer = playerOne;
+        }
+    }
+
+    private void initializeBoard() {
+        board = new int[49];
+        for (int i = 0; i < 49; i++) {
+                board[i] = 0;
+        }
+    }
+
+    public int[] getBoard() {
+        return board;
+    }
+
+    public void setBoard(int[] board) {
+        this.board = board;
+    }
+
+    public void putANut(Player player, int i) {
+        if (player == currentPlayer){
+            if (board[i] == 0){
+                board[i] = 1;
+            }
+        }
+    }
 }
