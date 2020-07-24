43b120a5fc9778b410f51a4835aef825712b0442
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-24 23:29:27 +0430

    fix scanner bug!

--- src/main/java/network/server/ClientHandler.java
+++ src/main/java/network/server/ClientHandler.java
@@ -1,6 +1,9 @@
 package network.server;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import network.request.Request;
 import network.request.SignUpRequest;
@@ -20,21 +23,25 @@ public class ClientHandler extends Thread {
     @Override
     public void run() {
         super.run();
-        while (!isInterrupted()){
             try {
                 Scanner scanner = new Scanner(socket.getInputStream());
-                while(true) {
-                    executeRequest(scanner.nextLine());
+                String requestString = null;
+                while (!isInterrupted()){
+                    System.out.println("waiting fo request...");
+                    requestString = scanner.nextLine();
+                    System.out.println(requestString);
+                    executeRequest(requestString);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
     }
 
     private void executeRequest(String jsonRequest) throws IOException {
+        System.out.println("executing request...");
         ObjectMapper objectMapper = new ObjectMapper();
-        Request request = objectMapper.readValue(jsonRequest, Request.class);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        SignUpRequest request = objectMapper.readValue(jsonRequest, SignUpRequest.class);
         if (request.getType().equals("SignUp")){
             SignUpRequest signUpRequest = objectMapper.readValue(jsonRequest, SignUpRequest.class);
             server.signUpPlayer(signUpRequest);
