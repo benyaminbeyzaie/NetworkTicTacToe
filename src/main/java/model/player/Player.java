0439e6402e2d0e70a8171ee7d951078ce33e5cb0
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-24 20:48:02 +0430

    update player fields

--- src/main/java/model/player/Player.java
+++ src/main/java/model/player/Player.java
@@ -1,4 +1,16 @@
 package model.player;
 
 public class Player {
+    private String username;
+    private String password;
+    private int score;
+
+    public Player(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setDefaults(){
+        score = 0;
+    }
 }
