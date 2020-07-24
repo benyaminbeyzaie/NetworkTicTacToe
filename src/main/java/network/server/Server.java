792b931ca005e6ac1f7579ab9a5ee4a20d77e1a1
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-24 20:48:34 +0430

    add sign up method to the server

--- src/main/java/network/server/Server.java
+++ src/main/java/network/server/Server.java
@@ -1,49 +1,79 @@
 package network.server;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import model.player.Player;
 import network.client.Client;
+import network.request.SignUpRequest;
 import view.config.configmodels.ServerConfig;
 import view.constants.Numbers;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
 
 public class Server extends Thread{
     private int port = Numbers.DEFAULT_PORT;
     private String ip = "localhost";
     private ServerSocket serverSocket;
-    private ArrayList<DataInputStream> inputStreams;
-    private ArrayList<DataOutputStream> outputStreams;
-    private boolean serverIsReadyToTakeClient = true;
+    private ArrayList<ClientHandler> clientHandlers;
 
     public Server(ServerConfig config) throws IOException {
         port = config.getPort();
         ip = config.getIp();
         serverSocket = new ServerSocket(config.getPort());
-        inputStreams = new ArrayList<>();
-        outputStreams = new ArrayList<>();
     }
+
+
 
     @Override
     public void run() {
         super.run();
+        System.out.println("Server started...");
         listenForJoinRequest();
     }
 
     private void listenForJoinRequest() {
         Socket socket = null;
         try {
-            while (serverIsReadyToTakeClient){
+            while (!isInterrupted()){
+                System.out.println("Listening for join clients...");
                 socket = serverSocket.accept();
-                inputStreams.add(new DataInputStream(socket.getInputStream()));
-                outputStreams.add(new DataOutputStream(socket.getOutputStream()));
-                System.out.println("CLIENT-" + inputStreams.size() + " HAS REQUESTED TO JOIN, AND WE HAVE ACCEPTED");
+                ClientHandler clientHandler = new ClientHandler(this, socket);
+                clientHandlers.add(clientHandler);
+                clientHandler.start();
+                System.out.println("CLIENT HAS REQUESTED TO JOIN, AND WE HAVE ACCEPTED");
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+
+    public synchronized int signUpPlayer(SignUpRequest signUpRequest) throws IOException {
+        if (signUpRequest.getUsername() == null || signUpRequest.getUsername().equals("")) return 0; // user is empty;
+        if (signUpRequest.getPassword() == null || signUpRequest.getPassword().equals("")) return 1; // pass is empty;
+        ObjectMapper objectMapper = new ObjectMapper();
+        //check if that file is exist or not!
+        final String PLAYER_FILE_PATH = "src/main/java/model/player/playerjsonfiles";
+        File file = new File(PLAYER_FILE_PATH + signUpRequest.getUsername() + ".json");
+        if (!file.exists()){
+            Player player = new Player(signUpRequest.getUsername(), signUpRequest.getPassword());
+            player.setDefaults();
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, player);
+            return 3; // User created successfully
+        }else {
+            return 2; // User exist
         }
     }
 }
