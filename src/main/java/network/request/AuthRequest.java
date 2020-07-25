e07251917ab681fb4d1f0291553d06efa5151598
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-25 21:43:12 +0430

    create auth base request

---
+++ src/main/java/network/request/AuthRequest.java
@@ -0,0 +1,22 @@
+package network.request;
+
+
+public abstract class AuthRequest extends Request {
+    String token;
+
+    public AuthRequest(String type, String token){
+        super(type);
+        this.token = token;
+    }
+
+    public AuthRequest() {
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
