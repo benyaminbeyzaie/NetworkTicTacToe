443aaa16f82696dd91dd06a1795273a89c2a3046
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-25 19:15:00 +0430

    add token and login method

--- src/main/java/network/server/ClientHandler.java
+++ src/main/java/network/server/ClientHandler.java
@@ -1,23 +1,21 @@
 package network.server;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import network.request.Request;
+import network.request.LoginRequest;
 import network.request.SignUpRequest;
 
-import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class ClientHandler extends Thread {
     Socket socket;
     Server server;
+    private String token = null;
 
-    ClientHandler(Server server, Socket socket){
+    ClientHandler(Server server, Socket socket) throws IOException {
         this.socket = socket;
         this.server = server;
     }
@@ -26,21 +24,21 @@ public class ClientHandler extends Thread {
         super.run();
             try {
                 Scanner scanner = new Scanner(socket.getInputStream());
-                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                PrintStream printStream = new PrintStream(socket.getOutputStream());
                 String requestString = null;
                 while (!isInterrupted()){
                     System.out.println("waiting fo request...");
                     requestString = scanner.nextLine();
-
-                    dataOutputStream.writeInt(executeRequest(requestString));
-                    dataOutputStream.flush();
+                    String response = executeRequest(requestString, this);
+                    printStream.println(response);
+                    printStream.flush();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
     }
 
-    private int executeRequest(String jsonRequest) throws IOException {
+    private String executeRequest(String jsonRequest, ClientHandler clientHandler) throws IOException {
         System.out.println("executing request...");
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -49,6 +47,18 @@ public class ClientHandler extends Thread {
             SignUpRequest signUpRequest = objectMapper.readValue(jsonRequest, SignUpRequest.class);
             return server.signUpPlayer(signUpRequest);
         }
-        return 0;
+        if (request.getType().equals("Login")){
+            LoginRequest loginRequest = objectMapper.readValue(jsonRequest, LoginRequest.class);
+            return server.signInPlayer(loginRequest, clientHandler);
+        }
+        return null;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) throws IOException {
+        this.token = token;
     }
 }
