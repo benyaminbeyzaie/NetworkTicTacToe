def62e0f9b957f2e7ae5f199a623573d9412e6ca
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-27 02:30:06 +0430

    add win/lose field and some methods

--- src/main/java/model/player/Player.java
+++ src/main/java/model/player/Player.java
@@ -5,12 +5,15 @@ import view.constants.Path;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Player {
     private String username;
     private String password;
     private int score;
     private int status;
+    private int wins;
+    private int loses;
 
     public Player(){}
     public Player(String username, String password) {
@@ -18,9 +21,34 @@ public class Player {
         this.password = password;
     }
 
+    public boolean checkPassword(String password) {
+        return password.equals(this.password);
+
+    }
+
+    public void savePlayerInfo() throws IOException {
+        File file = new File(Path.PLAYER_FILE_PATH + this.getUsername() + ".json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, this);
+    }
+
     public void setDefaults(){
         score = 0;
         status = 0; // offline
+        wins = 0;
+        loses = 0;
+    }
+
+    public boolean isIn(ArrayList<Player> players){
+        boolean t = false;
+        for (Player p:
+             players) {
+            if (p.getUsername().equals(username)){
+                t = true;
+                break;
+            }
+        }
+        return t;
     }
 
     public String getUsername() {
@@ -55,14 +83,19 @@ public class Player {
         this.status = status;
     }
 
-    public boolean checkPassword(String password) {
-        return password.equals(this.password);
-
+    public int getWins() {
+        return wins;
     }
 
-    public void savePlayerInfo() throws IOException {
-        File file = new File(Path.PLAYER_FILE_PATH + this.getUsername() + ".json");
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, this);
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLoses() {
+        return loses;
+    }
+
+    public void setLoses(int loses) {
+        this.loses = loses;
     }
 }
