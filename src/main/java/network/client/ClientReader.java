b8555600a19f4262798bd513d6a4075ebfd22ba2
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-29 20:35:00 +0430

    create client main

---
+++ src/main/java/network/client/ClientReader.java
@@ -0,0 +1,48 @@
+package network.client;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import network.response.LoginResponse;
+import network.response.Response;
+import network.response.SignUpResponse;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ClientReader extends Thread {
+    private Scanner scanner;
+    private ObjectMapper objectMapper;
+    private Client client;
+    ClientReader(DataInputStream dataInputStream, Client client){
+        scanner = new Scanner(dataInputStream);
+        this.client = client;
+        this.objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+    @Override
+    public void run() {
+        super.run();
+        while (!interrupted()){
+            try {
+                executeResponse(scanner.nextLine());
+                Thread.sleep(200);
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void executeResponse(String jsonResponse) throws IOException {
+        Response response = objectMapper.readValue(jsonResponse, Response.class);
+        if (response.getType().equals("SignUp")){
+            SignUpResponse signUpResponse = objectMapper.readValue(jsonResponse, SignUpResponse.class);
+            client.getStateManager().getStateContainer().getSignUpState().showDialogBox(signUpResponse.getStatus());
+        }
+        if (response.getType().equals("Login")){
+            LoginResponse loginResponse = objectMapper.readValue(jsonResponse, LoginResponse.class);
+            client.getStateManager().getStateContainer().getLoginState().showDialogBox(loginResponse.getStatus());
+        }
+    }
+}
