916fa4a133b3fc5d98bc0cb555d5b95ba0ec1169
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-29 20:35:29 +0430

    base edits on client handler

--- src/main/java/network/server/ClientHandler.java
+++ src/main/java/network/server/ClientHandler.java
@@ -3,70 +3,49 @@ package network.server;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import model.player.Player;
-import network.request.*;
+import network.request.LoginRequest;
+import network.request.Request;
+import network.request.SignUpRequest;
+import network.response.LoginResponse;
+import network.response.SignUpResponse;
+import view.constants.Path;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class ClientHandler extends Thread {
-    private Socket socket;
-    private Server server;
-    private Player  signedPlayer;
-    private String token = null;
+    private ServerWriter serverWriter;
+    private ServerReader serverReader;
+    private String token;
+    private ObjectMapper objectMapper;
+    private RequestExecutor requestExecutor;
+    private Player signedPlayer;
 
-    ClientHandler(Server server, Socket socket) throws IOException {
-        this.socket = socket;
-        this.server = server;
+    ClientHandler(Socket socket, Server server) throws IOException {
+        serverReader = new ServerReader(socket.getInputStream());
+        serverWriter = new ServerWriter(socket.getOutputStream());
+        objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        requestExecutor = new RequestExecutor(server, this);
     }
     @Override
     public void run() {
-        super.run();
+        while (!isInterrupted()){
             try {
-                Scanner scanner = new Scanner(socket.getInputStream());
-                PrintStream printStream = new PrintStream(socket.getOutputStream());
-                String requestString = null;
-                while (!isInterrupted()){
-                    System.out.println("waiting fo request...");
-                    requestString = scanner.nextLine();
-                    String response = executeRequest(requestString, this);
-                    printStream.println(response);
-                    printStream.flush();
-                }
-            } catch (IOException e) {
+                executeRequest(serverReader.read());
+                Thread.sleep(200);
+            } catch (InterruptedException | IOException e) {
                 e.printStackTrace();
             }
+        }
     }
 
-    private String executeRequest(String jsonRequest, ClientHandler clientHandler) throws IOException {
-        System.out.println("executing request...");
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        Request request = objectMapper.readValue(jsonRequest, SignUpRequest.class);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
-        if (request.getType().equals("SignUp")){
-            SignUpRequest signUpRequest = objectMapper.readValue(jsonRequest, SignUpRequest.class);
-            return server.signUpPlayer(signUpRequest);
-        }
-        if (request.getType().equals("Login")){
-            LoginRequest loginRequest = objectMapper.readValue(jsonRequest, LoginRequest.class);
-            return server.login(loginRequest, clientHandler);
-        }
-        if (request.getType().equals("GetSignedPlayerUsername")){
-            GetSignedPlayerUsernameRequest getSignedPlayerUsernameRequest = objectMapper.readValue(jsonRequest, GetSignedPlayerUsernameRequest.class);
-            return server.getSignedPlayerUsername(getSignedPlayerUsernameRequest, clientHandler);
-        }
-        if (request.getType().equals("GetAllPlayersNameWithStatus")){
-            GetAllPlayersNameWithStatusRequest getAllPlayersNameWithStatusRequest = objectMapper.readValue(jsonRequest, GetAllPlayersNameWithStatusRequest.class);
-            return server.getAllPlayersNameWithStatus(getAllPlayersNameWithStatusRequest, clientHandler);
-        }
-        if (request.getType().equals("LogOutRequest")){
-            LogOutRequest logOutRequest = objectMapper.readValue(jsonRequest, LogOutRequest.class);
-            return server.logOut(logOutRequest);
-        }
-        return null;
+    private void executeRequest(String jsonRequest) throws IOException {
+        requestExecutor.executeRequest(jsonRequest);
+        System.out.println(jsonRequest);
     }
+
 
     public String getToken() {
         return token;
@@ -74,6 +53,10 @@ public class ClientHandler extends Thread {
 
     public void setToken(String token) throws IOException {
         this.token = token;
+    }
+
+    public ServerWriter getServerWriter() {
+        return serverWriter;
     }
 
     public Player getSignedPlayer() {
