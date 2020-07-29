9ae2678ee7bf32bfcc2c1f6b451d09bb5168b0c0
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-29 23:52:42 +0430

    add username function

--- src/main/java/network/client/ClientReader.java
+++ src/main/java/network/client/ClientReader.java
@@ -3,6 +3,8 @@ package network.client;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import network.request.GetSignedPlayerUsernameRequest;
+import network.response.GetSignedPlayerUsernameResponse;
 import network.response.LoginResponse;
 import network.response.Response;
 import network.response.SignUpResponse;
@@ -35,6 +37,7 @@ public class ClientReader extends Thread {
     }
 
     private void executeResponse(String jsonResponse) throws IOException {
+        System.out.println("Response: " + jsonResponse );
         Response response = objectMapper.readValue(jsonResponse, Response.class);
         if (response.getType().equals("SignUp")){
             SignUpResponse signUpResponse = objectMapper.readValue(jsonResponse, SignUpResponse.class);
@@ -43,6 +46,10 @@ public class ClientReader extends Thread {
         if (response.getType().equals("Login")){
             LoginResponse loginResponse = objectMapper.readValue(jsonResponse, LoginResponse.class);
             client.getStateManager().getStateContainer().getLoginState().showDialogBox(loginResponse.getStatus());
+        }
+        if (response.getType().equals("Username")){
+            GetSignedPlayerUsernameResponse  getSignedPlayerUsernameResponse = objectMapper.readValue(jsonResponse, GetSignedPlayerUsernameResponse.class);
+            client.getStateManager().getStateContainer().getMenuState().getUsernameLabel().setText(getSignedPlayerUsernameResponse.getUsername());
         }
     }
 }
