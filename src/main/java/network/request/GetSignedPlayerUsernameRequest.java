86bd299e0acbcac7544a455fa7b15ced07955708
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-30 10:49:03 +0430

    edit super constructor

--- src/main/java/network/request/GetSignedPlayerUsernameRequest.java
+++ src/main/java/network/request/GetSignedPlayerUsernameRequest.java
@@ -1,18 +1,18 @@
 package network.request;
 
 public class GetSignedPlayerUsernameRequest extends AuthRequest {
-    public GetSignedPlayerUsernameRequest() {
-    }
 
-    public GetSignedPlayerUsernameRequest(String token) {
-        super("GetSignedPlayerUsername", token);
+    public GetSignedPlayerUsernameRequest() {}
+
+    public GetSignedPlayerUsernameRequest(String token){
+        super("Username" , token);
     }
 
     @Override
     public String toString() {
         return "{" +
-            "\"type\" : " + "\"" + getType()+ "\"," +
-                "\"token\" : " + "\"" + getToken()+ "\"" +
-                "}";
+                "\"type\" : " + "\"" + getType()+ "\"" +
+                ", \"token\" : " + "\"" +  getToken() + "\""  +
+                '}';
     }
 }
