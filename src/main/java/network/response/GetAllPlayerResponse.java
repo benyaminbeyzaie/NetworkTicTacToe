e2e547bdec39d3370b9a3b6a4c4034b90fcd22b3
Benyamin Beyzaie <benyamin137928@gmail.com>
2020-07-30 10:48:14 +0430

    complete all player response

--- src/main/java/network/response/GetAllPlayerResponse.java
+++ src/main/java/network/response/GetAllPlayerResponse.java
@@ -1,4 +1,30 @@
 package network.response;
 
-public class GetAllPlayerResponse {
+import network.request.GetAllPlayerRequest;
+
+public class GetAllPlayerResponse extends Response {
+    private String allPlayersByDash;
+
+    public GetAllPlayerResponse(){}
+
+    public GetAllPlayerResponse(String allPlayersByDash){
+        super("AllPlayer");
+        this.allPlayersByDash = allPlayersByDash;
+    }
+
+    public String getAllPlayersByDash() {
+        return allPlayersByDash;
+    }
+
+    public void setAllPlayersByDash(String allPlayersByDash) {
+        this.allPlayersByDash = allPlayersByDash;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"type\" : " + "\"" + getType()+ "\"" +
+                ", \"allPlayersByDash\" : " + "\"" +  getAllPlayersByDash() + "\""  +
+                '}';
+    }
 }
